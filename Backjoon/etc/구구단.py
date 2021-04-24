ece672a09bac636b84ee930d8bf9498d8202db0d
ElinaPark <elinapark0818@gmail.com>
2021-04-24 22:19:27 +0900

    구구단

---
+++ Backjoon/etc/구구단.py
@@ -0,0 +1,17 @@
+def printTimesTable(n):
+    result = []
+    i = 1
+    while i < 10:
+        result.append(n * i)
+        i += 1
+    return result
+
+
+print(printTimesTable(2))
+print(printTimesTable(3))
+print(printTimesTable(4))
+print(printTimesTable(5))
+print(printTimesTable(6))
+print(printTimesTable(7))
+print(printTimesTable(8))
+print(printTimesTable(9))
