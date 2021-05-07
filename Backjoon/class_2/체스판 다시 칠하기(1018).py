f5dcbcfa0eb845c410dd2a738cf3487f0c654529
ElinaPark <elinapark0818@gmail.com>
2021-05-07 16:50:04 +0900

    [8*8 보드] 이중 for 문으로 만들기
    첫 번째 색깔이 W인 경우
    첫 번째 색깔이 B인 경우
    비교해서 W 는 num1 에, B 는 num2 에 넣기
    
    색칠해야하는 칸의 최소값을 if 문으로 걸러낸다

--- Backjoon/class_2/체스판 다시 칠하기(1018).py
+++ Backjoon/class_2/체스판 다시 칠하기(1018).py
@@ -4,19 +4,36 @@
 # 체스판처럼 하기 위해 다시 칠해야 하는 칸의 최소 개수를 구하라
 # B=블랙 W=화이트
 
-# 8 8
-# WBWBWBWB
-# BWBWBWBW
-# WBWBWBWB
-# BWBBBWBW  BBB > BWB
-# WBWBWBWB
-# BWBWBWBW
-# WBWBWBWB
-# BWBWBWBW
+N, M = map(int, input().split())
+map_list = [list(input()) for _ in range(N)]
+min_num = None
 
-n, m = map(int, input().split())
+for i in range(N - 7):
+    for j in range(M - 7):
+        num1, num2 = 0, 0
 
-# n = len 값?
-# m = input 개수 값?
-# n = 8 and m = 8 로
+        for k in range(i, i + 8):
+            for l in range(j, j + 8):
+                if ((k + l - i - j) % 2 == 0):
+                    if (map_list[k][l] == 'B'):
+                        num1 += 1
+                else:
+                    if (map_list[k][l] == 'W'):
+                        num1 += 1
 
+        for k in range(i, i + 8):
+            for l in range(j, j + 8):
+                if ((k + l - i - j) % 2 == 0):
+                    if (map_list[k][l] == 'W'):
+                        num2 += 1
+                else:
+                    if (map_list[k][l] == 'B'):
+                        num2 += 1
+
+        change_box = num1 if num1 < num2 else num2
+        if (min_num is None):
+            min_num = change_box
+        else:
+            min_num = change_box if min_num > change_box else min_num
+
+print(min_num)
