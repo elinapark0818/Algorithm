10efd4e8f7dd8ce4c57b3a6fce59e53c3692d392
ElinaPark <elinapark0818@gmail.com>
2021-05-07 16:01:26 +0900

    체스판 다시 칠하기 N,M 값 받기

---
+++ Backjoon/class_2/체스판 다시 칠하기(1018).py
@@ -0,0 +1,22 @@
+# M*N 크기의 보드
+# M*N의 보드를 잘라서 8*8 로 만들자
+# 색깔은 흰색 또는 검은색
+# 체스판처럼 하기 위해 다시 칠해야 하는 칸의 최소 개수를 구하라
+# B=블랙 W=화이트
+
+# 8 8
+# WBWBWBWB
+# BWBWBWBW
+# WBWBWBWB
+# BWBBBWBW  BBB > BWB
+# WBWBWBWB
+# BWBWBWBW
+# WBWBWBWB
+# BWBWBWBW
+
+n, m = map(int, input().split())
+
+# n = len 값?
+# m = input 개수 값?
+# n = 8 and m = 8 로
+
