3301452bb346f68aa2a9708fec961f7abfdb76eb
ElinaPark <elinapark0818@gmail.com>
2021-05-07 17:54:18 +0900

    0이 아닐때, int 값의 팰린드롬 여부

--- Backjoon/class_2/체스판 다시 칠하기(1018).py
+++ Backjoon/class_2/체스판 다시 칠하기(1018).py
@@ -11,7 +11,6 @@ min_num = None
 for i in range(N - 7):
     for j in range(M - 7):
         num1, num2 = 0, 0
-
         for k in range(i, i + 8):
             for l in range(j, j + 8):
                 if ((k + l - i - j) % 2 == 0):
@@ -20,7 +19,6 @@ for i in range(N - 7):
                 else:
                     if (map_list[k][l] == 'W'):
                         num1 += 1
-
         for k in range(i, i + 8):
             for l in range(j, j + 8):
                 if ((k + l - i - j) % 2 == 0):
@@ -29,11 +27,10 @@ for i in range(N - 7):
                 else:
                     if (map_list[k][l] == 'B'):
                         num2 += 1
-
         change_box = num1 if num1 < num2 else num2
         if (min_num is None):
             min_num = change_box
         else:
             min_num = change_box if min_num > change_box else min_num
-
 print(min_num)
+
