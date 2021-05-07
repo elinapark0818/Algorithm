29b701808865285eee85e2291c0f694d8ca16f23
ElinaPark <elinapark0818@gmail.com>
2021-05-07 18:08:25 +0900

    체스판 다시 칠하기 좀 더 이해할 수 있는 풀이

--- Backjoon/class_2/체스판 다시 칠하기(1018).py
+++ Backjoon/class_2/체스판 다시 칠하기(1018).py
@@ -4,33 +4,59 @@
 # 체스판처럼 하기 위해 다시 칠해야 하는 칸의 최소 개수를 구하라
 # B=블랙 W=화이트
 
-N, M = map(int, input().split())
-map_list = [list(input()) for _ in range(N)]
-min_num = None
+# N, M = map(int, input().split())
+# map_list = [list(input()) for _ in range(N)]
+# min_num = None
+#
+# for i in range(N - 7):
+#     for j in range(M - 7):
+#         num1, num2 = 0, 0
+#         for k in range(i, i + 8):
+#             for l in range(j, j + 8):
+#                 if ((k + l - i - j) % 2 == 0):
+#                     if (map_list[k][l] == 'B'):
+#                         num1 += 1
+#                 else:
+#                     if (map_list[k][l] == 'W'):
+#                         num1 += 1
+#         for k in range(i, i + 8):
+#             for l in range(j, j + 8):
+#                 if ((k + l - i - j) % 2 == 0):
+#                     if (map_list[k][l] == 'W'):
+#                         num2 += 1
+#                 else:
+#                     if (map_list[k][l] == 'B'):
+#                         num2 += 1
+#         change_box = num1 if num1 < num2 else num2
+#         if (min_num is None):
+#             min_num = change_box
+#         else:
+#             min_num = change_box if min_num > change_box else min_num
+# print(min_num)
 
-for i in range(N - 7):
-    for j in range(M - 7):
-        num1, num2 = 0, 0
-        for k in range(i, i + 8):
-            for l in range(j, j + 8):
-                if ((k + l - i - j) % 2 == 0):
-                    if (map_list[k][l] == 'B'):
-                        num1 += 1
-                else:
-                    if (map_list[k][l] == 'W'):
-                        num1 += 1
-        for k in range(i, i + 8):
-            for l in range(j, j + 8):
-                if ((k + l - i - j) % 2 == 0):
-                    if (map_list[k][l] == 'W'):
-                        num2 += 1
-                else:
-                    if (map_list[k][l] == 'B'):
-                        num2 += 1
-        change_box = num1 if num1 < num2 else num2
-        if (min_num is None):
-            min_num = change_box
-        else:
-            min_num = change_box if min_num > change_box else min_num
-print(min_num)
+N = 8
+str1 = 'WBWBWBWB'
+str2 = 'BWBWBWBW'
+pivot1 = [str1, str2, str1, str2, str1, str2, str1, str2]
+pivot2 = [str2, str1, str2, str1, str2, str1, str2, str1]
 
+a, b = [int(x) for x in input().split()]
+arr = []
+for i in range(a):
+    arr.append(input())
+rst = float('inf')
+for i in range(a - N + 1):
+    for j in range(b - N + 1):
+        count = 0
+        for p in range(N):
+            for q in range(N):
+                if arr[i + p][j + q] != pivot1[p][q]:
+                    count += 1
+        rst = min(rst, count)
+        count = 0
+        for p in range(N):
+            for q in range(N):
+                if arr[i + p][j + q] != pivot2[p][q]:
+                    count += 1
+        rst = min(rst, count)
+print(rst)
