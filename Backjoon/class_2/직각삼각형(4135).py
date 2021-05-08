ad9d7f9b02188cc5d9c3f5ee17c4793ecaa8b57e
ElinaPark <elinapark0818@gmail.com>
2021-05-08 21:53:25 +0900

    직각삼각형 여부 확인하기 try - 2

--- Backjoon/class_2/직각삼각형(4135).py
+++ Backjoon/class_2/직각삼각형(4135).py
@@ -1,11 +1,14 @@
 # 제일 큰 수랑 제일 작은 수를 더해서 /2 한 값을 찾아라
+# 직각삼각형이 맞다면 right 아니라면 wrong
+# 0 0 0 이 입력되면 break
 
 while True:
-    n = map(int, input().split())
-    result = 0
-    if n == '0 0 0':
+    n = list(map(int, input().split()))
+    a = max(n)
+    b = min(n)
+    if a == 0:
         break
-    if ((max(n) + min(n)) / 2) == n:
+    if ((a + b) % 2) == 0:
         print('right')
     else:
         print('wrong')
