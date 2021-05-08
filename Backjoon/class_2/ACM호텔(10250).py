eb90ca8136deb55ee42522af51f9f70665564ff0
ElinaPark <elinapark0818@gmail.com>
2021-05-08 23:42:52 +0900

    연산자 //(몫) 로 쉽게 구할 수 있었다.

--- Backjoon/class_2/ACM호텔(10250).py
+++ Backjoon/class_2/ACM호텔(10250).py
@@ -13,3 +13,43 @@
 # H > N 이면, H-N 만큼 x축이 +=1
 # H < N 이면, N만큼 y축이 += 1
 # N = (x,y)
+
+# T = int(input())
+# for i in range(T):
+#     H, W, N = map(int, input().split())
+#     room = N
+#     floor = N % H
+#     if N % H == 0:
+#         room = N
+#         floor = H
+#     print({floor * 100 + room})
+
+
+# for _ in range(int(input())):
+#     h, w, n = map(int, input().split())
+#     a = n % h
+#     b = n
+#     if a == 0:
+#         a = h
+#         b -= 1
+#     print(a * 100 + b)
+
+# t 입력
+t = int(input())
+for i in range(t):
+    # h,w,n 입력
+    h, w, n = map(int, input().split())
+    # x축은 n을 h로 나눴을때의 몫+1
+    x = n // h + 1
+    # n(사람 수)이 h(층 수)로 나누어질때
+    if n % h == 0:
+        y = h
+        # x축은 n을 h로 나눈 몫이다
+        x = n // h
+    # n(사람 수)이 h(층 수)로 나누어지지 않는다면 층수만 올라간다
+    else:
+        # y축은 층수이다
+        y = n % h
+    # 결과값은 층수가 100자리부터 시작하니까 *100 하고서 x축 값을 더한것
+    result = y * 100 + x
+    print(result)
