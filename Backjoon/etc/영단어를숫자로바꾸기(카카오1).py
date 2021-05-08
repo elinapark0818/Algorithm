540639f1823fbb92104f4389a728686aa73752bb
ElinaPark <elinapark0818@gmail.com>
2021-05-08 15:17:17 +0900

    딕셔너리 키값 바꾸기 고민해보았다.

---
+++ Backjoon/etc/영단어를숫자로바꾸기(카카오1).py
@@ -0,0 +1,22 @@
+# dicts = [{0: 'zero', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven',
+#           8: 'eight', 9: 'nine'}]
+# n가 p에게 숫자+영단어 카드를 건넨다
+# p가 숫자+영단어를 숫자로 바꿔서 출력한다
+
+
+# if you have string and integer
+# for item in dicts:
+#     for key, value in item.iter():
+#         try:
+#             item[key] = int(value)
+#         except ValueError:
+#             item[key] = str(value)
+
+
+# list = [{'a': 1, 'b': 2, 'c': 3}, {'d': 4, 'e': 5, 'f': 6}]
+#
+# for sub in list:
+#     for key in sub:
+#         sub[key] = int(sub[key])
+#         print(list)
+
