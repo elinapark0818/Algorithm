7867c39baa091b99c20e16e42d2e64516e32b23a
ElinaPark <elinapark0818@gmail.com>
2021-05-09 23:52:36 +0900

    1. for i in range(1,N+1)로 1부터 최대범위까지 모든 경우의 수(브루트 포스)를 처음부터 끝까지 비교한다. 2. N을 str형으로 입력받고 list에 append 3. if i + sum(i) == N 이면, print(i)

--- Backjoon/class_2/분해합(2231).py
+++ Backjoon/class_2/분해합(2231).py
@@ -4,6 +4,7 @@
 # m = int(x) + int(n)
 # print(m)
 
+
 # n = int(input())
 # a = 0
 #
@@ -17,6 +18,7 @@
 #     if i == n:
 #         print(0)
 
+
 # n = int(input())
 # for i in range(n+1):
 #     i_str = str(i)
@@ -28,19 +30,32 @@
 #
 # print(i if i != n else 0)
 
+# 함수 이름이 길어서 헷갈리넹..
+# N = int(input())  # 목표 분해합
+# low = 0
+#
+# def get_devided_num(low_n):
+#     e = list(map(int, str(low_n)))
+#     devided_num = low_n + sum(e)
+#     return devided_num
+#
+# while get_devided_num(low) != N:
+#     if low == N:
+#         low = 0
+#         break
+#     else:
+#         low += 1
+# print(low)
 
-N = int(input())  # 목표 분해합
-low = 0
 
-def get_devided_num(low_n):
-    e = list(map(int, str(low_n)))
-    devided_num = low_n + sum(e)
-    return devided_num
+N = int(input())
+result = 0
 
-while get_devided_num(low) != N:
-    if low == N:
-        low = 0
+for i in range(1, N+1):
+    a = list(map(int, str(i)))
+    s = i + sum(a)
+    if(s == N):
+        result = i
         break
-    else:
-        low += 1
-print(low)
+
+print(result)
