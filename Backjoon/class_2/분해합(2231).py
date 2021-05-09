293be70fbb984a62b5bf38e908b7aac8228bb1c4
ElinaPark <elinapark0818@gmail.com>
2021-05-09 23:38:16 +0900

    브루트포스

--- Backjoon/class_2/분해합(2231).py
+++ Backjoon/class_2/분해합(2231).py
@@ -1,6 +1,19 @@
 # 분해합(m) = 256(245+2+4+5) 생성자(x)= 245
-x = input()
-n = sum(map(int, str(x)))
-m = int(x) + int(n)
-print(m)
+# x = input()
+# n = sum(map(int, str(x)))
+# m = int(x) + int(n)
+# print(m)
+
+n = int(input())
+a = 0
+
+for i in range(1, n + 1):
+    m = list(map(int, str(i)))
+    a = i + sum(m)
+
+    if a == n:
+        print(i)
+        break
+    if i == n:
+        print(0)
 
