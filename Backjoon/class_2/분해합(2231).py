f257a7db64f8797525a85898e6dc5b138588a61a
ElinaPark <elinapark0818@gmail.com>
2021-05-09 23:16:50 +0900

    이걸 토대로 가장 작은 생성자를 구해내보자

--- Backjoon/class_2/분해합(2231).py
+++ Backjoon/class_2/분해합(2231).py
@@ -1,5 +1,6 @@
-# 분해합 = 256(245+2+4+5) 생성자= 245
-m = input()
-n = sum(map(int, str(m)))
-x = int(m) + int(n)
-print(x)
+# 분해합(m) = 256(245+2+4+5) 생성자(x)= 245
+x = input()
+n = sum(map(int, str(x)))
+m = int(x) + int(n)
+print(m)
+
