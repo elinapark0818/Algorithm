a60917ade7bcb75d6dea36a9ddb60ddb4c74d3e8
ElinaPark <elinapark0818@gmail.com>
2021-05-11 00:32:15 +0900

    부녀회장 낼 다시 풀어봐야징

--- Backjoon/class_2/부녀회장(2775).py
+++ Backjoon/class_2/부녀회장(2775).py
@@ -5,17 +5,27 @@
 
 
 # k층 n호에 몇 명이 살고 있는지 출력하라.
+#
+# T = int(input())
+#
+# for _ in range(T):
+#     k = int(input())  # 층
+#     n = int(input())  # 호
+#     result = [i for i in range(1, n + 1)]  # 제일 아래층 사람 수
+#
+#     for _ in range(k):  # 층 수만큼 반복
+#         for q in range(1, n):  # 1 ~ number-1 까지
+#             print(result[q])
+#             result[q] += result[q - 1]  # 층 별 각 호실의 사람 수를 변경
+#         print(result)
+#     print(result[-1])  # 가장 마지막 수 출력
 
-T = int(input())
-
-for _ in range(T):
-    k = int(input())  # 층
-    n = int(input())  # 호
-    result = [i for i in range(1, n + 1)]  # 제일 아래층 사람 수
-
-    for _ in range(k):  # 층 수만큼 반복
-        for q in range(1, n):  # 1 ~ number-1 까지
-            print(result[q])
-            result[q] += result[q - 1]  # 층 별 각 호실의 사람 수를 변경
-        print(result)
-    print(result[-1])  # 가장 마지막 수 출력
+t = int(input())
+for _ in range(t):
+    k = int(input())
+    n = int(input())
+    rst = [i for i in range(1, n + 1)]
+    for _ in range(k):
+        for q in range(1, n):
+            rst[q] += rst[q - 1]
+    print(rst[-1])
