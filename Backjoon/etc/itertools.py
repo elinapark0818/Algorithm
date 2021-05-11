adf941adb4306520d0bd2c1b9a094214b65c8228
ElinaPark <elinapark0818@gmail.com>
2021-05-11 15:21:26 +0900

    itertools.permutations 다른 예제

--- Backjoon/etc/itertools.py
+++ Backjoon/etc/itertools.py
@@ -10,3 +10,8 @@ print(list(map(''.join, itertools.permutations(arr, 3))))
 
 # 2개의 원소로 순열 만들기
 print(list(map(''.join, itertools.permutations(arr, 2))))
+
+# 2개의 원소로 순열 만들기 (1)
+int_arr = ['1', '2', '3']
+nPr = itertools.permutations(int_arr, 2)
+print(list(nPr))
