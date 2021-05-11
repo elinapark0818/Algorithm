ee67cae237fc76a6d256c32420cff051681a9116
ElinaPark <elinapark0818@gmail.com>
2021-05-11 15:17:31 +0900

    순열! permutations

--- Backjoon/etc/itertools.py
+++ Backjoon/etc/itertools.py
@@ -2,11 +2,11 @@ import itertools
 
 arr = ['A', 'B', 'C', 'D']
 
-# 원소 개수만큼 (여기서는 4) 수열 만들기
+# 원소 개수만큼 (여기서는 4) 순열 만들기
 print(list(map(''.join, itertools.permutations(arr))))
 
-# 3개의 원소로 수열 만들기
+# 3개의 원소로 순열 만들기
 print(list(map(''.join, itertools.permutations(arr, 3))))
 
-# 2개의 원소로 수열 만들기
+# 2개의 원소로 순열 만들기
 print(list(map(''.join, itertools.permutations(arr, 2))))
