974b478130d90ccde2e74627ff34d95a0e0832ea
ElinaPark <elinapark0818@gmail.com>
2021-05-12 21:28:23 +0900

    설탕 배달

--- Backjoon/class_2/설탕배달(2839).py
+++ Backjoon/class_2/설탕배달(2839).py
@@ -12,15 +12,15 @@ while True:
         break
 
 # 다른 풀이
-
-N = int(input())
-count = 0
-while N >= 0:
-    if N % 5 == 0:
-        count += (N // 5)
-        print(count)
-        break
-    N -= 3
-    count += 1
-else:
-    print(-1)
+#
+# N = int(input())
+# count = 0
+# while N >= 0:
+#     if N % 5 == 0:
+#         count += (N // 5)
+#         print(count)
+#         break
+#     N -= 3
+#     count += 1
+# else:
+#     print(-1)
