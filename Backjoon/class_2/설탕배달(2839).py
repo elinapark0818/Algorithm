b1ef8bf600b3cb7b475cf2d90746d530b75db8ac
ElinaPark <elinapark0818@gmail.com>
2021-05-12 21:27:29 +0900

    설탕 배달

---
+++ Backjoon/class_2/설탕배달(2839).py
@@ -0,0 +1,26 @@
+N = int(input())
+cnt = 0
+while True:
+    if (N % 5) == 0:
+        cnt += (N // 5)
+        print(cnt)
+        break
+    N -= 3
+    cnt += 1
+    if N < 0:
+        print(-1)
+        break
+
+# 다른 풀이
+
+N = int(input())
+count = 0
+while N >= 0:
+    if N % 5 == 0:
+        count += (N // 5)
+        print(count)
+        break
+    N -= 3
+    count += 1
+else:
+    print(-1)
