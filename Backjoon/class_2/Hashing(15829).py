d036f6dbb6567eb61e5bc72cb719a738e748b787
ElinaPark <elinapark0818@gmail.com>
2021-05-12 21:03:01 +0900

    Hassing 다시 풀어보기

--- Backjoon/class_2/Hashing(15829).py
+++ Backjoon/class_2/Hashing(15829).py
@@ -1,12 +1,27 @@
-# 항의 번호에 해당하는만큼 특정한 숫자를 거듭제곱한 것을 곱한다음 더하는 것
+# 항의 번호에 해당하는만큼 특정한 숫자를 거듭제곱한 것을 곱한 다음 더하는 것
 
-L = int(input())
-str = input()
-result = 0
-for i in range(L):
-    result += (ord(str[i])-96)*(31**i)
-print(result % 1234567891)
+# a = 1 로 만들어야 함
+# r = 31 로 만들어야 함
+# M = 1234567891 이다
+# abcde의 해시 값은 1 × 310 + 2 × 311 + 3 × 312 + 4 × 313 + 5 × 314 = 1 + 62 + 2883 + 119164 + 4617605 = 4739715이다.
+
+
+# L = int(input())
+# str = input()
+# result = 0
+# for i in range(L):
+#     result += (ord(str[i]) - 96) * (31 ** i)
+# print(result % 1234567891)
 
 # ord() : 문자를 아스키코드 값으로 반환
-# a = 97 이기 때문에 ord(str[i])-96 = 1 이 되도록 96을 빼준다
+# a = 97 이기 때문에 1 이 되도록 96을 빼준다
 # r = 31 이기 때문에 (31**i) 를 곱해준다
+# 적당히 큰 수 M으로 나눠주자.
+
+L = int(input())
+low = input()
+rst = 0
+for i in range(L):
+    M = 1234567891
+    rst += (ord(low[i]) - 96) * (31 ** i)
+print(rst % M)
