88d4705a9af93182a624c3eb3300793ce5a413ba
ElinaPark <elinapark0818@gmail.com>
2021-05-14 00:08:55 +0900

    팰린드롬 수 (sys, collections)

--- Backjoon/class_2/팰린드롬수(1259).py
+++ Backjoon/class_2/팰린드롬수(1259).py
@@ -1,8 +1,35 @@
+# while True:
+#     n = input()
+#     if n == '0':
+#         break
+#     if n == n[::-1]:
+#         print('yes')
+#     else:
+#         print('no')
+
+import sys
+import collections
+
 while True:
-    n = input()
-    if n == '0':
+    case = sys.stdin.readline().strip()
+
+    if case == "0":
         break
-    if n == n[::-1]:
-        print('yes')
-    else:
-        print('no')
+
+    deque = collections.deque(list(map(int, case)))
+
+    while True:
+        if len(deque) == 1:
+            print("yes")
+            break
+
+        left = deque.popleft()
+        right = deque.pop()
+
+        if left != right:
+            print("no")
+            break
+
+        if left == right and len(deque) == 0:
+            print("yes")
+            break
