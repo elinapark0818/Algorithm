0d53c8d6fc3b544bf9abe1227a8eb65f8a5e0ddc
ElinaPark <elinapark0818@gmail.com>
2021-05-16 01:39:10 +0900

    4중 for문

--- Backjoon/class_2/체스판 다시 칠하기(1018).py
+++ Backjoon/class_2/체스판 다시 칠하기(1018).py
@@ -34,29 +34,66 @@
 #             min_num = change_box if min_num > change_box else min_num
 # print(min_num)
 
-N = 8
-str1 = 'WBWBWBWB'
-str2 = 'BWBWBWBW'
-pivot1 = [str1, str2, str1, str2, str1, str2, str1, str2]
-pivot2 = [str2, str1, str2, str1, str2, str1, str2, str1]
+# N = 8
+# str1 = 'WBWBWBWB'
+# str2 = 'BWBWBWBW'
+# pivot1 = [str1, str2, str1, str2, str1, str2, str1, str2]
+# pivot2 = [str2, str1, str2, str1, str2, str1, str2, str1]
+#
+# a, b = [int(x) for x in input().split()]
+# arr = []
+# for i in range(a):
+#     arr.append(input())
+# rst = float('inf')
+# for i in range(a - N + 1):
+#     for j in range(b - N + 1):
+#         count = 0
+#         for p in range(N):
+#             for q in range(N):
+#                 if arr[i + p][j + q] != pivot1[p][q]:
+#                     count += 1
+#         rst = min(rst, count)
+#         count = 0
+#         for p in range(N):
+#             for q in range(N):
+#                 if arr[i + p][j + q] != pivot2[p][q]:
+#                     count += 1
+#         rst = min(rst, count)
+# print(rst)
 
-a, b = [int(x) for x in input().split()]
-arr = []
-for i in range(a):
-    arr.append(input())
-rst = float('inf')
-for i in range(a - N + 1):
-    for j in range(b - N + 1):
-        count = 0
-        for p in range(N):
-            for q in range(N):
-                if arr[i + p][j + q] != pivot1[p][q]:
-                    count += 1
-        rst = min(rst, count)
-        count = 0
-        for p in range(N):
-            for q in range(N):
-                if arr[i + p][j + q] != pivot2[p][q]:
-                    count += 1
-        rst = min(rst, count)
-print(rst)
+
+# 4중 for문
+N, M = map(int, input().split())
+board = list()
+for i in range(N):
+    board.append(input())
+repair = list()
+
+# i, j 로 8*8 크기 조절 = (N-i)*(M-j)
+for i in range(N - 7):
+    for j in range(M - 7):
+        # 화이트로 시작한 경우 와 블랙으로 시작한 경우의 값을 초기화 = 0
+        first_W = 0
+        first_B = 0
+        # 처음 자른 체스판부터 for 시작한다
+        for q in range(i, i + 8):
+            for p in range(j, j + 8):
+                # 행과 열의 인덱스의 합이 짝수인 경우, 일정한 한 색을 가지게 되고
+                # 홀수인 경우에도 다른 일정한 한 색을 가지게 된다.
+                if (q + p) % 2 == 0:
+                    if board[q][p] != 'W':
+                        first_W += 1
+                    if board[q][p] != 'B':
+                        first_B += 1
+                else:
+                    if board[q][p] != 'B':
+                        first_W += 1
+                    if board[q][p] != 'W':
+                        first_B += 1
+        # 화이트로 시작했을 때 바꿔야 할 부분
+        repair.append(first_W)
+        # 블랙으로 시작했을 때 바꿔야 할 부분
+        repair.append(first_B)
+
+# 바꿔야 할 개수의 최소값
+print(min(repair))
--- Backjoon/etc/pivot_table.py
+++ Backjoon/etc/pivot_table.py
@@ -3,6 +3,18 @@
 # - Group 연산, 테이블 요약, 그래프 등을 위해 사용할 수 있다
 # - set_index 로 계층적 색인 생성 후, unstack method로 형태를 변경하는 과정의 축약형이다
 
+
+# pivot_table(data, values=None, index=None, columns=None, aggfunc='mean', fill_value=None, margins=False, margins_name='All')
+# data: 분석할 데이터프레임 (메서드일 때는 필요하지 않음)
+# values: 분석할 데이터프레임에서 분석할 열
+# index: 행 인덱스로 들어갈 키 열 또는 키 열의 리스트
+# columns: 열 인덱스로 들어갈 키 열 또는 키 열의 리스트
+# aggfunc: 분석 메서드
+# fill_value: NaN 대체 값
+# margins: 모든 데이터를 분석한 결과를 오른쪽과 아래에 붙일지 여부
+# margins_name: 마진 열(행)의 이름
+
+
 import pandas as pd
 
 df = pd.DataFrame([
@@ -25,3 +37,28 @@ df_pivoted2.columns = df_pivoted2.columns.values
 df_pivoted2.reset_index(level=0, inplace=True)
 
 print(df_pivoted2)
+
+
+print("--------------------------")
+
+data = {
+    "도시": ["서울", "서울", "서울", "부산", "부산", "부산", "인천", "인천"],
+    "연도": ["2015", "2010", "2005", "2015", "2010", "2005", "2015", "2010"],
+    "인구": [9904312, 9631482, 9762546, 3448737, 3393191, 3512547, 2890451, 263203],
+    "지역": ["수도권", "수도권", "수도권", "경상권", "경상권", "경상권", "수도권", "수도권"]
+}
+columns = ["도시", "연도", "인구", "지역"]
+df1 = pd.DataFrame(data, columns=columns)
+print(df1)
+
+print("--------------------------")
+
+print(df1.set_index(["도시", "연도"])[["인구"]].unstack())
+
+print("--------------------------")
+
+print(df1["인구"].groupby([df1["지역"], df1["연도"]]).sum().unstack("연도"))
+
+print("--------------------------")
+
+print(df1.pivot_table("인구", "도시", "연도", margins=True, margins_name="합계"))
