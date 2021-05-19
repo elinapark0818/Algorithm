47cdfd51ccc61defbbb769dfa1e2f678ab10286b
ElinaPark <elinapark0818@gmail.com>
2021-05-19 23:48:34 +0900

    최대공약수 와 최소공배수

--- Backjoon/class_2/최대공약수와최소공배수(2609).py
+++ Backjoon/class_2/최대공약수와최소공배수(2609).py
@@ -1,5 +1,15 @@
 a, b = map(int, input().split())
 
+for i in range(a + 1, 1, -1):
+    if a % i == 0 and b % i == 0:
+        print(i)
+        break
+
+for i in range(b,(a*b)+1):
+    if i % a == 0 and i % b == 0:
+        print(i)
+        break
+
 
 def gcd(a, b):
     while b > 0:
