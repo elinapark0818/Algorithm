19a240a9e1c7355de6986335796be4fca387f858
ElinaPark <elinapark0818@gmail.com>
2021-05-19 23:53:49 +0900

    유클리드 호제법을 알아보자

--- Backjoon/class_2/최대공약수와최소공배수(2609).py
+++ Backjoon/class_2/최대공약수와최소공배수(2609).py
@@ -1,11 +1,15 @@
+import sys
+
 a, b = map(int, input().split())
 
+# 최대공약수 구하기
 for i in range(a + 1, 1, -1):
     if a % i == 0 and b % i == 0:
         print(i)
         break
 
-for i in range(b,(a*b)+1):
+# 최소공배수 구하기
+for i in range(b, (a * b) + 1):
     if i % a == 0 and i % b == 0:
         print(i)
         break
@@ -21,3 +25,25 @@ def gcd(a, b):
 
 def lcm(a, b):
     return a * b / gcd(a, b)
+
+
+
+# 유클리드 호제법으로 풀기
+
+a, b = map(int, input().split())
+
+
+def gcd(a, b):
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a % b)
+
+
+def lcm(a, b):
+    g = gcd(a, b)
+    return int(g * (a / g) * (b / g))
+
+
+print(gcd(a, b))
+print(lcm(a, b))
