0f985a7cac6a8a9faa1e058bd9e905018c5d10ba
ElinaPark <elinapark0818@gmail.com>
2021-05-23 23:27:09 +0900

    좌표 정렬하기

--- Backjoon/class_2/좌표 정렬하기(11650).py
+++ Backjoon/class_2/좌표 정렬하기(11650).py
@@ -1,18 +1,19 @@
 import sys
-
-N = int(sys.stdin.readline())
-M = []
-for i in range(N):
-    M.append(list(map(int, sys.stdin.readline().split())))
-    M.sort(key=lambda x: (x[0], x[1]))
-
-for i in M:
-    print(i[0], i[1])
+#
+# N = int(sys.stdin.readline())
+# M = []
+# for i in range(N):
+#     M.append(list(map(int, sys.stdin.readline().split())))
+#     M.sort(key=lambda x: (x[0], x[1]))
+#
+# for i in M:
+#     print(i[0], i[1])
 
 # 다른 풀이
 
 n = int(sys.stdin.readline())
 arr = []
+
 for i in range(n):
     [a, b] = map(int, sys.stdin.readline().split())
     arr.append([a, b])
@@ -25,11 +26,11 @@ for j in range(n):
 
 N = int(sys.stdin.readline())
 
-arr = []
+array = []
 for i in range(N):
     [x, y] = map(int, sys.stdin.readline().split())
-    arr.append([x, y])
-result = sorted(arr)
+    array.append([x, y])
+result = sorted(array)
 
 for j in range(N):
     print(result[j][0], result[j][1])
