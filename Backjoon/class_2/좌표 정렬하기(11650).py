9ecbb5b3f92e596277d051dd3bec5003af4f6fb4
ElinaPark <elinapark0818@gmail.com>
2021-05-23 23:09:27 +0900

    좌표 정렬하기

--- Backjoon/class_2/좌표 정렬하기(11650).py
+++ Backjoon/class_2/좌표 정렬하기(11650).py
@@ -1,5 +1,11 @@
 import sys
 
 N = int(sys.stdin.readline())
+M = []
 for _ in range(N):
     x, y = map(int, sys.stdin.readline().split())
+    M.append((x, y))
+    M.sort()
+
+for i in M:
+    print(i[0], i[1])
