f8ec7bd76bbbbdfbd659b78d1ff29c6df6f4a018
ElinaPark <elinapark0818@gmail.com>
2021-05-23 22:55:22 +0900

    import sys 적용

--- Backjoon/class_2/수 정렬3(10989).py
+++ Backjoon/class_2/수 정렬3(10989).py
@@ -1,9 +1,26 @@
-N = int(input())
-li = []
+# 시간 초과
+# N = int(input())
+# li = []
+#
+# for i in range(N):
+#     li.append(list(input().split()))
+#     li.sort()
+#
+# for i in li:
+#     print(i[0])
+
+import sys
+
+N = int(sys.stdin.readline())
+
+li = [0] * 10001
 
 for i in range(N):
-    li.append(list(input().split()))
-    li.sort()
+    input_num = int(sys.stdin.readline())
 
-for i in li:
-    print(i[0])
+    li[input_num] = li[input_num] + 1
+
+for i in range(10001):
+    if li[i] != 0:
+        for j in range(li[i]):
+            print(i)
