d235256eb69ef4d98841fb55990f3e5ec08be573
ElinaPark <elinapark0818@gmail.com>
2021-05-23 23:22:17 +0900

    좌표 정렬하기 sorted!

--- Backjoon/class_2/좌표 정렬하기(11650).py
+++ Backjoon/class_2/좌표 정렬하기(11650).py
@@ -2,10 +2,21 @@ import sys
 
 N = int(sys.stdin.readline())
 M = []
-for _ in range(N):
-    x, y = map(int, sys.stdin.readline().split())
-    M.append((x, y))
+for i in range(N):
+    M.append(list(map(int, sys.stdin.readline().split())))
     M.sort(key=lambda x: (x[0], x[1]))
 
 for i in M:
     print(i[0], i[1])
+
+# 다른 풀이
+
+n = int(sys.stdin.readline())
+arr = []
+for i in range(n):
+    [a, b] = map(int, sys.stdin.readline().split())
+    arr.append([a, b])
+rst = sorted(arr)
+
+for j in range(n):
+    print(rst[j][0], rst[j][1])
