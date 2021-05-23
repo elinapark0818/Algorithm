f27e1530018b58546feab989f1dac8955be1fd34
ElinaPark <elinapark0818@gmail.com>
2021-05-23 23:13:10 +0900

    좌표 정렬하기 lambda 적용

--- Backjoon/class_2/좌표 정렬하기(11650).py
+++ Backjoon/class_2/좌표 정렬하기(11650).py
@@ -5,7 +5,7 @@ M = []
 for _ in range(N):
     x, y = map(int, sys.stdin.readline().split())
     M.append((x, y))
-    M.sort()
+    M.sort(key=lambda x: (x[0], x[1]))
 
 for i in M:
     print(i[0], i[1])
