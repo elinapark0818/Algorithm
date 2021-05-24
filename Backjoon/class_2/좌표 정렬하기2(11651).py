8608cedb72082703e3272a5b0d873aee2ec36fe1
ElinaPark <elinapark0818@gmail.com>
2021-05-24 22:27:16 +0900

    좌표 정렬하기2

--- Backjoon/class_2/좌표 정렬하기2(11651).py
+++ Backjoon/class_2/좌표 정렬하기2(11651).py
@@ -1,3 +1,13 @@
 import sys
 
 N = int(sys.stdin.readline())
+array = []
+
+for i in range(N):
+    xy = list(map(int, sys.stdin.readline().split()))
+    array.append(xy)
+
+array.sort(key=lambda x: (x[1], x[0]))
+
+for i in array:
+    print(i[0], i[1])
