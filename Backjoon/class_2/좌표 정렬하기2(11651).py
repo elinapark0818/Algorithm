ea9919b1c6eaf1eabd9b2ad1b568a5db7ab0caef
ElinaPark <elinapark0818@gmail.com>
2021-05-25 22:39:38 +0900

    좌표 정렬하기2 lambda

--- Backjoon/class_2/좌표 정렬하기2(11651).py
+++ Backjoon/class_2/좌표 정렬하기2(11651).py
@@ -9,3 +9,15 @@ arr.sort(key=lambda x: (x[1], x[0]))
 
 for i in arr:
     print(i[0], i[1])
+
+N = int(sys.stdin.readline())
+arr = []
+
+for i in range(N):
+    x, y = map(int, sys.stdin.readline().split())
+    arr.append([y, x])
+
+rst = sorted(arr)
+
+for y, x in rst:
+    print(x, y)
