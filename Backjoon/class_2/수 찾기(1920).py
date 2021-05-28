e7cb4b591683d99036f6847c362dbeb63bf0658b
ElinaPark <elinapark0818@gmail.com>
2021-05-29 00:05:50 +0900

    sys 모듈

--- Backjoon/class_2/수 찾기(1920).py
+++ Backjoon/class_2/수 찾기(1920).py
@@ -8,3 +8,33 @@
 # for i in M:
 #     sys.stdout.write('1\n') if 1 in N else sys.stdout.write('0\n')
 
+
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+s = list(map(int, input().split()))
+m = int(input())
+s_ = list(map(int, input().split()))
+s.sort()
+for i in s_:
+    low, high = 0, n
+    while low <= high:
+        mid = (low + high) // 2
+        if mid < n and mid > -1:
+            if s[mid] < i:
+                low = mid + 1
+            else:
+                high = mid - 1
+        else:
+            break
+    if mid < n and mid > -1:
+        if i == s[high + 1]:
+            print(1)
+        else:
+            print(0)
+    else:
+        print(0)
+
+
+
