04545c6bd4437725392fe1b7f555193c96cfb798
ElinaPark <elinapark0818@gmail.com>
2021-05-28 23:57:22 +0900

    수 찾기

---
+++ Backjoon/class_2/수 찾기(1920).py
@@ -0,0 +1,10 @@
+# import sys
+#
+# n = sys.stdin.readline()
+# N = set(sys.stdin.readline().split())
+# m = sys.stdin.readline()
+# M = sys.stdin.readline().split()
+#
+# for i in M:
+#     sys.stdout.write('1\n') if 1 in N else sys.stdout.write('0\n')
+
