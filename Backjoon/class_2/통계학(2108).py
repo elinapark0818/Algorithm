65df33516e6bbcde738b8aae3bd3582462634740
ElinaPark <elinapark0818@gmail.com>
2021-06-03 00:25:45 +0900

    ㅜㅜ 런타임에러라니.. 이유를 찾도록 하자

--- Backjoon/class_2/통계학(2108).py
+++ Backjoon/class_2/통계학(2108).py
@@ -1,26 +1,20 @@
 import sys
+from collections import Counter
 
 N = int(sys.stdin.readline())
 nums = [int(sys.stdin.readline()) for _ in range(N)]
 
 nums.sort()
 
+k = Counter(nums).most_common()
+
 print(round(sum(nums) / N))
-
-# round() : 반올림하는 내장함수
-
 print(nums[N // 2])
 
-# 최빈값 : 빈도가 가장 많은 수 + 여러개일 경우 두 번재로 작은 값 출력
-from collections import Counter
-
-k = Counter(nums).most_common()
 if len(nums) > 1:
     if k[0][1] == k[1][1]:
         print(k[1][0])
     else:
-        print(nums[0])
-
-# 범위구하기 : 마지막꺼에서 첫번째꺼 빼편 되겠쮸
+        print(nums[0][0])
 
 print(nums[-1] - nums[0])
