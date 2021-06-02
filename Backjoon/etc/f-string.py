1961fcc762b8ae89f48880bb7aaac80843b9d161
ElinaPark <elinapark0818@gmail.com>
2021-06-03 00:07:51 +0900

    f-string 알아보기

--- Backjoon/etc/f-string.py
+++ Backjoon/etc/f-string.py
@@ -6,3 +6,29 @@ s = 'coffe'
 n = 5
 result = f'저는 {s}를 좋아합니다. 하루에 {n}잔 마셔요.'
 print(result)
+
+#
+#
+#
+
+# f-string 왼쪽 정렬
+s1 = 'left'
+result1 = f'|{s1:<10}|'
+print(result1)
+
+# f-string 가운데 정렬
+s2 = 'mid'
+result2 = f'|{s2:^10}|'
+print(result2)
+
+# f-string 오른쪽 정렬
+s3 = 'right'
+result3 = f'|{s3:>10}|'
+print(result3)
+
+# f-string 중괄호 출력 :
+# 2개 = 중괄호 자체
+# 3개 = {string 값}
+num = 10
+rst = f'my age {{{num}}}, {{num}}'
+print(rst)
