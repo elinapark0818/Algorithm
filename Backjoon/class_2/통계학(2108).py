6b68b06b73904b5face40e689af0780745366147
ElinaPark <elinapark0818@gmail.com>
2021-06-03 00:33:57 +0900

    찾았따! 꼼꼼히 확인하자!

--- Backjoon/class_2/통계학(2108).py
+++ Backjoon/class_2/통계학(2108).py
@@ -8,13 +8,16 @@ nums.sort()
 
 k = Counter(nums).most_common()
 
-print(round(sum(nums) / N))
+print("%.f" % (sum(nums) / N))
+# print(round(sum(nums) / N))
 print(nums[N // 2])
 
 if len(nums) > 1:
     if k[0][1] == k[1][1]:
         print(k[1][0])
     else:
-        print(nums[0][0])
+        print(k[0][0])
+else:
+    print(nums[0])
 
 print(nums[-1] - nums[0])
