27577fcf258884bca98e1b1e6466150d43da9692
ElinaPark <elinapark0818@gmail.com>
2021-06-03 00:16:03 +0900

    from collections import Counter

--- Backjoon/class_2/통계학(2108).py
+++ Backjoon/class_2/통계학(2108).py
@@ -5,6 +5,19 @@ nums = [int(sys.stdin.readline()) for _ in range(N)]
 
 nums.sort()
 
-print(round(sum(nums)/N))
+print(round(sum(nums) / N))
 
 # round() : 반올림하는 내장함수
+
+print(nums[N // 2])
+
+# 최빈값 : 빈도가 가장 많은 수 + 여러개일 경우 두 번재로 작은 값 출력
+from collections import Counter
+
+k = Counter(nums).most_common()
+if len(nums) > 1:
+    if k[0][1] == k[1][1]:
+        print(k[1][0])
+    else:
+        print(nums[0])
+
