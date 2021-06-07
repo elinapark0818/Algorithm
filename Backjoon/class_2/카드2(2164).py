58af410b3469c4657ca7264745c06d42de3fc8db
ElinaPark <elinapark0818@gmail.com>
2021-06-07 23:43:14 +0900

    카드2 다른풀이

--- Backjoon/class_2/카드2(2164).py
+++ Backjoon/class_2/카드2(2164).py
@@ -1,11 +1,22 @@
-import sys
-import collections
+# import sys
+# import collections
+#
+# N = int(sys.stdin.readline())
+# rst = collections.deque([i for i in range(1, N + 1)])
+#
+# while len(rst) > 1:
+#     rst.popleft()
+#     rst.rotate(-1)
+#
+# print(rst[0])
 
-N = int(sys.stdin.readline())
-rst = collections.deque([i for i in range(1, N + 1)])
-
-while len(rst) > 1:
-    rst.popleft()
-    rst.rotate(-1)
-
-print(rst[0])
+a = int(input())
+b = 2
+while True:
+    if a == 1 or a == 2:
+        print(a)
+        break
+    b *= 2
+    if b >= a:
+        print((a - (b // 2)) * 2)
+        break
