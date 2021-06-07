255d1d8b4d276a3972f8b6c972b09ba867d49eca
ElinaPark <elinapark0818@gmail.com>
2021-06-07 23:36:38 +0900

    카드2

--- Backjoon/class_2/카드2(2164).py
+++ Backjoon/class_2/카드2(2164).py
@@ -1,3 +1,11 @@
 import sys
+import collections
 
 N = int(sys.stdin.readline())
+rst = collections.deque([i for i in range(1, N + 1)])
+
+while len(rst) > 1:
+    rst.popleft()
+    rst.rotate(-1)
+
+print(rst[0])
