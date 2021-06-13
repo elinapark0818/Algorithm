6ec11d89be2864a721191468e4dc75032e706f2c
ElinaPark <elinapark0818@gmail.com>
2021-06-13 22:45:52 +0900

    괄호 틀렸따..

--- Backjoon/class_2/괄호(9012).py
+++ Backjoon/class_2/괄호(9012).py
@@ -1,5 +1,25 @@
 N = int(input())
-while True:
-    for i in range(N):
-        vps = input()
-        rst = []
+rst = []
+for i in range(N):
+    inputList = list(input())
+    stack = []
+    answer = True
+
+    for j in range(len(inputList)):
+        if inputList[j] == "(":
+            stack.append(inputList[j])
+        else:
+            try:
+                if stack.pop() == "(":
+                    pass
+            except:
+                rst.append("no")
+                answer = False
+                break
+    if len(stack):
+        rst.append("no")
+        continue
+    if answer:
+        rst.append("yes")
+for i in rst:
+    print(i)
