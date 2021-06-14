ff906fe7d14a87a739a4bae4a197a7d1bc36c786
ElinaPark <elinapark0818@gmail.com>
2021-06-14 23:05:10 +0900

    Hashing

--- Backjoon/class_2/숫자카드2(10816).py
+++ Backjoon/class_2/숫자카드2(10816).py
@@ -1,10 +1,16 @@
-N = int(input())
-for i in range(N):
-    nums = input().split()
-    rst = 0
+# Hashing
 
-    M = int(input())
-    for j in range(M):
-        card = input().split()
+from sys import stdin
 
+_ = stdin.readline()
+N = map(int, stdin.readline().split())
+_ = stdin.readline()
+M = map(int, stdin.readline().split())
+hashmap = {}
+for i in N:
+    if i in hashmap:
+        hashmap[i] += 1
+    else:
+        hashmap[i] = 1
+print(' '.join(str(hashmap[j]) if j in hashmap else '0' for j in M))
 
