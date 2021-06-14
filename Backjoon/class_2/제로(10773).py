ea0047026d9db0f99acca6355d70d8f319a0cd10
ElinaPark <elinapark0818@gmail.com>
2021-06-14 22:38:05 +0900

    단순하게 생각해보면 ...

--- Backjoon/class_2/제로(10773).py
+++ Backjoon/class_2/제로(10773).py
@@ -1,1 +1,10 @@
 K = int(input())
+for i in K:
+    num = list(int(input()))
+    rst = []
+    for j in num:
+        if j != 0:
+            rst.append(j)
+        if j == 0:
+            num.pop()
+    print(sum(int(rst)))
