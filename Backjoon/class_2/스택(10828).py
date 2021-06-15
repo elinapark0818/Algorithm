c3333235b9a6a36c2e9ee22320185e9259a0615a
ElinaPark <elinapark0818@gmail.com>
2021-06-15 23:59:04 +0900

    스택(10828)

--- Backjoon/class_2/스택(10828).py
+++ Backjoon/class_2/스택(10828).py
@@ -1,50 +1,78 @@
+# import sys
+#
+# # 정수 X를 스택에 넣는 연산이다.
+# def push(x):
+#     stack.append(x)
+#
+#
+# # 스택에서 가장 위에 있는 정수를 빼고, 그 수를 출력한다.
+# # 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+# def pop():
+#     if (not stack):
+#         return -1
+#     else:
+#         return stack.pop()
+#
+#
+# # 스택에 들어있는 정수의 개수를 출력한다.
+# def size():
+#     return len(stack)
+#
+#
+# # 스택이 비어있으면 1, 아니면 0을 출력한다.
+# def empty():
+#     return 0 if stack else 1
+#
+#
+# # 스택의 가장 위에 있는 정수를 출력한다.
+# # 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+# def top():
+#     return stack[-1] if stack else -1
+#
+#
+# N = int(sys.stdin.readline().rstrip())
+# stack = []
+#
+# for _ in range(N):
+#     input_split = sys.stdin.readline().rstrip().split()
+#
+#     order = input_split[0]
+#
+#     if order == "push":
+#         push(input_split[1])
+#     elif order == "pop":
+#         print(pop())
+#     elif order == "size":
+#         print(size())
+#     elif order == "empty":
+#         print(empty())
+#     elif order == "top":
+#         print(top())
+
 import sys
 
-# 정수 X를 스택에 넣는 연산이다.
-def push(x):
-    stack.append(x)
+n = int(sys.stdin.readline())
 
-
-# 스택에서 가장 위에 있는 정수를 빼고, 그 수를 출력한다.
-# 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
-def pop():
-    if (not stack):
-        return -1
-    else:
-        return stack.pop()
-
-
-# 스택에 들어있는 정수의 개수를 출력한다.
-def size():
-    return len(stack)
-
-
-# 스택이 비어있으면 1, 아니면 0을 출력한다.
-def empty():
-    return 0 if stack else 1
-
-
-# 스택의 가장 위에 있는 정수를 출력한다.
-# 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
-def top():
-    return stack[-1] if stack else -1
-
-
-N = int(sys.stdin.readline().rstrip())
 stack = []
+for i in range(n):
+    command = sys.stdin.readline().split()
 
-for _ in range(N):
-    input_split = sys.stdin.readline().rstrip().split()
-
-    order = input_split[0]
-
-    if order == "push":
-        push(input_split[1])
-    elif order == "pop":
-        print(pop())
-    elif order == "size":
-        print(size())
-    elif order == "empty":
-        print(empty())
-    elif order == "top":
-        print(top())
+    if command[0] == 'push':
+        stack.append(command[1])
+    elif command[0] == 'pop':
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack.pop())
+    elif command[0] == 'size':
+        print(len(stack))
+    elif command[0] == 'empty':
+        if len(stack) == 0:
+            print(1)
+        else:
+            print(0)
+    elif command[0] == 'top':
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack[-1])
