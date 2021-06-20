d1646e285b31e458b173e7943b5e2cdd69a9d6fa
Elina <elinapark0818@gmail.com>
2021-06-20 18:56:49 +0900

    큐(10845)

--- Backjoon/class_2/큐(10845).py
+++ Backjoon/class_2/큐(10845).py
@@ -1,3 +1,27 @@
-import sys
-
-n = int(sys.stdin.readline())
+from sys import stdin
+qu = []
+for _ in range(int(stdin.readline())):
+    array = stdin.readline().split()
+    if array[0] == 'push':
+        qu.append(array[1])
+    elif array[0] == 'pop':
+        if qu:
+            print(qu.pop(0))
+        else:
+            print(-1)
+    elif array[0] == 'size':
+        print(len(qu))
+    elif array[0] == 'empty':
+        print(1 - int(bool(qu)))
+    elif array[0] == 'front':
+        if qu:
+            print(qu[0])
+        else:
+            print(-1)
+    elif array[0] == 'back':
+        if qu:
+            print(qu[-1])
+        else:
+            print(-1)
+    else:
+        pass
