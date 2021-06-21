9b6193896fe9fcfc2153725d7bbe2d5cb2d2c749
Elina <elinapark0818@gmail.com>
2021-06-21 23:48:02 +0900

    appandleft

--- Backjoon/class_2/덱(10866).py
+++ Backjoon/class_2/덱(10866).py
@@ -1,7 +1,23 @@
+from collections import deque
 import sys
 
 n = int(sys.stdin.readline())
 
-deque = []
+dq = deque()
+
+
+def empty():
+    if len(dq) == 0:
+        return 1
+    else:
+        return 0
+
+
+def size():
+    return len(dq)
+
+
 for i in range(n):
-    deq = sys.stdin.readline().split()
+    command = list(sys.stdin.readline().split())
+    if command[0] == 'push_front':
+        dq.appendleft(command[1])
