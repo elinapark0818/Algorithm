fde67ebb55d84b4960a80bfa61151ddf4418658c
Elina <elinapark0818@gmail.com>
2021-06-21 23:57:48 +0900

    큐/덱 다른풀이

--- Backjoon/class_2/덱(10866).py
+++ Backjoon/class_2/덱(10866).py
@@ -1,47 +1,91 @@
-from collections import deque
+# from collections import deque
+# import sys
+#
+# n = int(sys.stdin.readline())
+# dq = deque()
+#
+# def empty():
+#     if len(dq) == 0:
+#         return 1
+#     else:
+#         return 0
+#
+# def size():
+#     return len(dq)
+#
+# for i in range(n):
+#     command = list(sys.stdin.readline().split())
+#     if command[0] == 'push_front':
+#         dq.appendleft(command[1])
+#     elif command[0] == 'push_back':
+#         dq.append(command[1])
+#     elif command[0] == 'pop_front':
+#         if empty() == 1:
+#             print('-1')
+#         else:
+#             tmp = dq.popleft()
+#             print(tmp)
+#     elif command[0] == 'pop_back':
+#         if empty() == 1:
+#             print('-1')
+#         else:
+#             tmp = dq.pop()
+#             print(tmp)
+#     elif command[0] == 'front':
+#         if empty() == 1:
+#             print('-1')
+#         else:
+#             print(dq[0])
+#     elif command[0] == 'back':
+#         if empty() == 1:
+#             print('-1')
+#         else:
+#             print(dq[len(dq) - 1])
+#     elif command[0] == 'size':
+#         print(size())
+#     elif command[0] == 'empty':
+#         print(empty())
+
+# 다른 풀이
+
 import sys
 
-n = int(sys.stdin.readline())
-dq = deque()
-
-def empty():
-    if len(dq) == 0:
-        return 1
-    else:
-        return 0
-
-def size():
-    return len(dq)
-
+input = sys.stdin.readline
+deque = []
+n = int(input())
 for i in range(n):
-    command = list(sys.stdin.readline().split())
-    if command[0] == 'push_front':
-        dq.appendleft(command[1])
-    elif command[0] == 'push_back':
-        dq.append(command[1])
-    elif command[0] == 'pop_front':
-        if empty() == 1:
-            print('-1')
+    command = input().rstrip()
+    if " " in command:
+        a, b = command.split()
+        if a == 'push_front':
+            deque.insert(0, b)
+        elif a == 'push_back':
+            deque.append(b)
+    elif "pop_front" == command:
+        if len(deque) == 0:
+            print(-1)
         else:
-            tmp = dq.popleft()
-            print(tmp)
-    elif command[0] == 'pop_back':
-        if empty() == 1:
-            print('-1')
+            print(deque.pop(0))
+    elif "pop_back" == command:
+        if len(deque) == 0:
+            print(-1)
         else:
-            tmp = dq.pop()
-            print(tmp)
-    elif command[0] == 'front':
-        if empty() == 1:
-            print('-1')
+            print(deque.pop(-1))
+
+    elif 'size' == command:
+        print(len(deque))
+    elif 'empty' == command:
+        if len(deque) == 0:
+            print(1)
         else:
-            print(dq[0])
-    elif command[0] == 'back':
-        if empty() == 1:
-            print('-1')
+            print(0)
+    elif 'front' == command:
+        if len(deque) == 0:
+            print(-1)
         else:
-            print(dq[len(dq) - 1])
-    elif command[0] == 'size':
-        print(size())
-    elif command[0] == 'empty':
-        print(empty())
+            print(deque[0])
+    elif 'back' == command:
+        if len(deque) == 0:
+            print(-1)
+        else:
+            print(deque[-1])
--- Backjoon/class_2/큐(10845).py
+++ Backjoon/class_2/큐(10845).py
@@ -1,29 +1,68 @@
+# from sys import stdin
+#
+# qu = []
+#
+# for _ in range(int(stdin.readline())):
+#     array = stdin.readline().split()
+#     if array[0] == 'push':
+#         qu.append(array[1])
+#     elif array[0] == 'pop':
+#         if qu:
+#             print(qu.pop(0))
+#         else:
+#             print(-1)
+#     elif array[0] == 'size':
+#         print(len(qu))
+#     elif array[0] == 'empty':
+#         print(1 - int(bool(qu)))
+#     elif array[0] == 'front':
+#         if qu:
+#             print(qu[0])
+#         else:
+#             print(-1)
+#     elif array[0] == 'back':
+#         if qu:
+#             print(qu[-1])
+#         else:
+#             print(-1)
+#     else:
+#         pass
+
+# 다른 풀이
+
 from sys import stdin
 
-qu = []
+N = int(stdin.readline())
+Que = []
+for i in range(N):
+    A = stdin.readline().split()
 
-for _ in range(int(stdin.readline())):
-    array = stdin.readline().split()
-    if array[0] == 'push':
-        qu.append(array[1])
-    elif array[0] == 'pop':
-        if qu:
-            print(qu.pop(0))
+    if A[0] == 'push':
+        Que.append(A[1])
+
+    elif A[0] == 'pop':
+        if Que:
+            print(Que.pop(0))
         else:
             print(-1)
-    elif array[0] == 'size':
-        print(len(qu))
-    elif array[0] == 'empty':
-        print(1 - int(bool(qu)))
-    elif array[0] == 'front':
-        if qu:
-            print(qu[0])
+
+    elif A[0] == 'size':
+        print(len(Que))
+
+    elif A[0] == 'empty':
+        if len(Que) == 0:
+            print(1)
         else:
+            print(0)
+
+    elif A[0] == 'front':
+        if len(Que) == 0:
             print(-1)
-    elif array[0] == 'back':
-        if qu:
-            print(qu[-1])
         else:
+            print(Que[0])
+
+    elif A[0] == 'back':
+        if len(Que) == 0:
             print(-1)
-    else:
-        pass
+        else:
+            print(Que[-1])
