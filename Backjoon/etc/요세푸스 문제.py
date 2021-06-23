122135c08de0c8e82fd8b863646167338cac3e87
Elina <elinapark0818@gmail.com>
2021-06-23 23:04:44 +0900

    요세푸스 문제

--- Backjoon/etc/요세푸스 문제.py
+++ Backjoon/etc/요세푸스 문제.py
@@ -1,14 +1,43 @@
+# N, K = map(int, input().split())
+# q = []
+# result = []
+#
+# for i in range(K):
+#     q.append(i + 1)
+#     c = 0
+#
+# while len(q) > 0:
+#     c = (c + (K - 1)) % len(q)
+#     cc = q.pop(c)
+#     result.append(str(cc))
+#
+# print("<%s>" % (", ".join(result)))
+
+
+# 다른 풀이
+# 입력
 N, K = map(int, input().split())
-q = []
+
+josephus = [i for i in range(1, N + 1)]
 result = []
 
-for i in range(K):
-    q.append(i + 1)
-    c = 0
+temp = K - 1
 
-while len(q) > 0:
-    c = (c + (K - 1)) % len(q)
-    cc = q.pop(c)
-    result.append(str(cc))
+for i in range(N):
+    if len(josephus) > temp:  # 위치가 리스트를 넘지 않은 경우
+        result.append(josephus.pop(temp))  # 답 추가
+        temp += K - 1  # 다음 위치로 이동
 
-print("<%s>" % (", ".join(result)))
+    elif len(josephus) <= temp:  # 위치가 리스트를 넘은 경우
+        temp = temp % len(josephus)
+        result.append(josephus.pop(temp))
+        temp += K - 1
+
+# 출력
+print("<", end='')
+for i in result:
+    if i == result[-1]:
+        print(i, end='')
+    else:
+        print("%s, " % (i), end='')
+print(">")
