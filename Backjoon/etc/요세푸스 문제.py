03bfe1b95231c8eb8557e3a3c1f74eadc08e155c
Elina <elinapark0818@gmail.com>
2021-06-23 23:08:44 +0900

    요세푸스 다른풀이

--- Backjoon/etc/요세푸스 문제.py
+++ Backjoon/etc/요세푸스 문제.py
@@ -15,29 +15,42 @@
 
 
 # 다른 풀이
-# 입력
-N, K = map(int, input().split())
+# # 입력
+# N, K = map(int, input().split())
+#
+# josephus = [i for i in range(1, N + 1)]
+# result = []
+#
+# temp = K - 1
+#
+# for i in range(N):
+#     if len(josephus) > temp:  # 위치가 리스트를 넘지 않은 경우
+#         result.append(josephus.pop(temp))  # 답 추가
+#         temp += K - 1  # 다음 위치로 이동
+#
+#     elif len(josephus) <= temp:  # 위치가 리스트를 넘은 경우
+#         temp = temp % len(josephus)
+#         result.append(josephus.pop(temp))
+#         temp += K - 1
+#
+# # 출력
+# print("<", end='')
+# for i in result:
+#     if i == result[-1]:
+#         print(i, end='')
+#     else:
+#         print("%s, " % (i), end='')
+# print(">")
 
-josephus = [i for i in range(1, N + 1)]
+
+n, k = map(int, input().split())
+
+circle = [i for i in range(1, n + 1)]
 result = []
+num = 0
 
-temp = K - 1
+while len(result) != n:
+    num = (num + (k - 1)) % len(circle)
+    result.append(circle.pop(num))
 
-for i in range(N):
-    if len(josephus) > temp:  # 위치가 리스트를 넘지 않은 경우
-        result.append(josephus.pop(temp))  # 답 추가
-        temp += K - 1  # 다음 위치로 이동
-
-    elif len(josephus) <= temp:  # 위치가 리스트를 넘은 경우
-        temp = temp % len(josephus)
-        result.append(josephus.pop(temp))
-        temp += K - 1
-
-# 출력
-print("<", end='')
-for i in result:
-    if i == result[-1]:
-        print(i, end='')
-    else:
-        print("%s, " % (i), end='')
-print(">")
+print("<%s>" % (", ".join(map(str, result))))
