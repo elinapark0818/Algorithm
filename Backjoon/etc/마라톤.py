dc9576775667752d3083af96608e1a353cf72484
Elina <elinapark0818@gmail.com>
2021-06-24 12:43:24 +0900

    hash 로 푼 코드

--- Backjoon/etc/마라톤.py
+++ Backjoon/etc/마라톤.py
@@ -1,9 +1,35 @@
+# def solution(participant, completion):
+#     participant.sort()
+#     completion.sort()
+#
+#     for i in range(len(completion)):
+#         if participant[i] != completion[i]:
+#             return participant[i]
+#
+#     return participant[-1]
+
+# import collections
+#
+#
+# def solution(participant, completion):
+#     participant.sort()
+#     completion.sort()
+#     result = collections.Counter(participant) - collections.Counter(completion)
+#
+#     return list(result)[0]
+
+# 다른 코드
 def solution(participant, completion):
-    participant.sort()
-    completion.sort()
+    answer = ''
+    temp = 0
+    dic = {}
 
-    for i in range(len(completion)):
-        if participant[i] != completion[i]:
-            return participant[i]
+    for p in participant:
+        dic[hash(p)] = p
+        temp += int(hash(p))
 
-    return participant[-1]
+    for c in completion:
+        temp -= hash(c)
+        answer = dic[temp]
+
+    return answer
