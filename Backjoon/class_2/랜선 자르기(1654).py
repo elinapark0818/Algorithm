ef73e6502fb636f68c3e9faa2732a939b758e2ef
Elina <elinapark0818@gmail.com>
2021-06-28 23:47:13 +0900

    랜선자르기

--- Backjoon/class_2/랜선 자르기(1654).py
+++ Backjoon/class_2/랜선 자르기(1654).py
@@ -1,9 +1,22 @@
-
 # 첫째 줄에는 K, N
 # 1 <= K <= 10,000
 # 1 <= N <= 1,000,000
 # K <= N
 
-K, N = int(input().split())
+import sys
 
+K, N = map(int, input().split())
+lan = [int(sys.stdin.readline()) for _ in range(K)]
+start, end = 1, max(lan)
 
+while start <= end:
+    mid = (start + end) // 2
+    lines = 0
+    for i in lan:
+        lines += i // mid
+
+    if lines >= N:
+        start = mid + 1
+    else:
+        end = mid - 1
+print(end)
