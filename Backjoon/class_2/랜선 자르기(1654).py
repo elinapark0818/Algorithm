e89be699e269a38f197def3b092713bd489caf7d
Elina <elinapark0818@gmail.com>
2021-07-01 18:48:02 +0900

    랜선자르기(1654)

--- Backjoon/class_2/랜선 자르기(1654).py
+++ Backjoon/class_2/랜선 자르기(1654).py
@@ -2,21 +2,43 @@
 # 1 <= K <= 10,000
 # 1 <= N <= 1,000,000
 # K <= N
+#
+# import sys
+#
+# K, N = map(int, sys.stdin.readline().split())
+# lan = [int(sys.stdin.readline()) for _ in range(K)]
+# start, end = 1, max(lan)
+#
+# while start <= end:
+#     mid = (start + end) // 2
+#     lines = 0
+#     for i in lan:
+#         lines += i // mid
+#
+#     if lines >= N:
+#         start = mid + 1
+#     else:
+#         end = mid - 1
+# print(end)
 
 import sys
 
-K, N = map(int, input().split())
-lan = [int(sys.stdin.readline()) for _ in range(K)]
-start, end = 1, max(lan)
+k, n = map(int, sys.stdin.readline().split())
+arr = []
+for _ in range(k):
+    arr.append(int(sys.stdin.readline()))
+l, h = 1, max(arr)
+result = 0
 
-while start <= end:
-    mid = (start + end) // 2
-    lines = 0
-    for i in lan:
-        lines += i // mid
+while l <= h:
+    middle = (l + h) // 2
+    countSum = sum([(i // middle) for i in arr])
 
-    if lines >= N:
-        start = mid + 1
-    else:
-        end = mid - 1
-print(end)
+    if countSum >= n:
+        result = middle
+        l = middle + 1
+
+    elif countSum < n:
+        h = middle - 1
+
+print(result)
