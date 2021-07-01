aae9cfe740fd6e86154fefc6e822df1929e711ed
Elina <elinapark0818@gmail.com>
2021-07-01 18:30:32 +0900

    리스트를 슬라이싱하면 그대로 리스트를 리턴한다!!

--- 프로그래머스/반복숫자제거.py
+++ 프로그래머스/반복숫자제거.py
@@ -25,13 +25,24 @@
 
 
 # 다시 풀어보기
+#
+# def solution(arr):
+#     answer = []
+#     for i in range(len(arr)):
+#         if arr[i - 1] != arr[i]:
+#             answer.append(arr[i])
+#         elif i == 0:
+#             answer.append(arr[i])
+#
+#     return answer
+
+
+# 다른 사람들이 많이 공감한 코드
+# 리스트 슬라이싱하면 그대로 리스트를 리턴한다.
 
 def solution(arr):
     answer = []
-    for i in range(len(arr)):
-        if arr[i - 1] != arr[i]:
-            answer.append(arr[i])
-        elif i == 0:
-            answer.append(arr[i])
-
+    for i in arr:
+        if answer[-1:] == [i]: continue
+        answer.append(i)
     return answer
