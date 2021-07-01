4dac3a14fecba5ec2c19d8137255f24b9865027f
Elina <elinapark0818@gmail.com>
2021-07-01 18:18:26 +0900

    반복숫자는 싫어 다시 풀어보기

--- 프로그래머스/반복숫자제거.py
+++ 프로그래머스/반복숫자제거.py
@@ -1,24 +1,37 @@
+# # def solution(arr):
+# #     answer = []
+# #
+# #     for i in range(len(arr)):
+# #         if arr[i - 1] != arr[i]:
+# #             answer.append(arr[i])
+# #         elif i == 0:
+# #             answer.append(arr[i])
+# #
+# #     return answer
+#
+# # 다른 코드
 # def solution(arr):
 #     answer = []
-#
-#     for i in range(len(arr)):
-#         if arr[i - 1] != arr[i]:
-#             answer.append(arr[i])
-#         elif i == 0:
-#             answer.append(arr[i])
-#
+#     for i in arr:
+#         if answer[-1:] == [i]:
+#             continue
+#         answer.append(i)
 #     return answer
+#
+#
+# # 다른 코드
+# def no_continuous(s):
+#     return [a for b, a in enumerate(s) if a != s[b - 1]]
 
-# 다른 코드
+
+# 다시 풀어보기
+
 def solution(arr):
     answer = []
-    for i in arr:
-        if answer[-1:] == [i]:
-            continue
-        answer.append(i)
+    for i in range(len(arr)):
+        if arr[i - 1] != arr[i]:
+            answer.append(arr[i])
+        elif i == 0:
+            answer.append(arr[i])
+
     return answer
-
-
-# 다른 코드
-def no_continuous(s):
-    return [a for b, a in enumerate(s) if a != s[b - 1]]
