a4748ce8d65d33d7f0b48490b1bbf193ddc32c23
Elina <elinapark0818@gmail.com>
2021-07-01 18:50:07 +0900

    랜선자르기(1654) 다른풀이 직관적이길래

--- Backjoon/class_2/랜선 자르기(1654).py
+++ Backjoon/class_2/랜선 자르기(1654).py
@@ -42,3 +42,20 @@ while l <= h:
         h = middle - 1
 
 print(result)
+
+
+
+# 다른 풀이
+
+# input = sys.stdin.readline
+# k, n = map(int, input().split())
+# s = []
+# for i in range(k): s.append(int(input()))
+# low, high = 0, 10000000000
+# while low <= high:
+#     mid = (low + high) // 2
+#     num = 0
+#     for i in s: num += i // mid
+#     if num >= n: low = mid + 1
+#     else: high = mid - 1
+# print(high)
