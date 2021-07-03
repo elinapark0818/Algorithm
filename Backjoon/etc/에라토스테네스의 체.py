3c40b4a7743195121d196634aab762f619c94f5a
Elina <elinapark0818@gmail.com>
2021-07-03 21:20:58 +0900

    에라토스테네스의 체(소수)

--- Backjoon/etc/에라토스테네스의 체.py
+++ Backjoon/etc/에라토스테네스의 체.py
@@ -1,10 +1,31 @@
-n = 1000
-a = [False, False] + [True] * (n - 1)
-primes = []
+# n = 1000
+# a = [False, False] + [True] * (n - 1)
+# primes = []
+#
+# for i in range(2, n + 1):
+#     if a[i]:
+#         primes.append(i)
+#         for j in range(2 * i, n + 1, i):
+#             a[j] = False
+# print(primes)
 
+# 다른 방법
+
+import math
+
+n = 1000  # 2부터 1,000까지의 모든 수에 대하여 소수 판별
+array = [True for i in range(n + 1)]  # 처음엔 모든 수가 소수(True)인 것으로 초기화
+
+# 에라토스테네스의 체 알고리즘
+for i in range(2, int(math.sqrt(n)) + 1):  # 2부터 n의 제곱근까지의 모든 수를 확인하며
+    if array[i] == True:  # i가 소수인 경우 (남은 수인 경우)
+        # i를 제외한 i의 모든 배수를 지우기
+        j = 2
+        while i * j <= n:
+            array[i * j] = False
+            j += 1
+
+# 모든 소수 출력
 for i in range(2, n + 1):
-    if a[i]:
-        primes.append(i)
-        for j in range(2 * i, n + 1, i):
-            a[j] = False
-print(primes)
+    if array[i]:
+        print(i, end=' ')
