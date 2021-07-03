b486649774c211d23f557e4c70288bd72e9d817e
Elina <elinapark0818@gmail.com>
2021-07-03 21:28:02 +0900

    정말 간략한 에라토스테네스의 체

--- 프로그래머스/소수 찾기.py
+++ 프로그래머스/소수 찾기.py
@@ -1,8 +1,17 @@
+# def solution(n):
+#     answer = set([i for i in range(3, n + 1, 2)])
+#
+#     for i in range(3, n + 1, 2):
+#         if i in answer:
+#             answer -= set([i for i in range(i * 2, n + 1, i)])
+#
+#     return len(answer) + 1
+
+
 def solution(n):
-    answer = set([i for i in range(3, n + 1, 2)])
+    num = set(range(2, n + 1))
 
-    for i in range(3, n + 1, 2):
-        if i in answer:
-            answer -= set([i for i in range(i * 2, n + 1, i)])
-
-    return len(answer) + 1
+    for i in range(2, n + 1):
+        if i in num:
+            num -= set(range(2 * i, n + 1, i))
+    return len(num)
