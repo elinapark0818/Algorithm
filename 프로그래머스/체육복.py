957860c1ee78e945ee233bb969fec358c1985487
Elina <elinapark0818@gmail.com>
2021-07-03 20:20:22 +0900

    막 써보기

--- 프로그래머스/체육복.py
+++ 프로그래머스/체육복.py
@@ -1,3 +1,17 @@
+# n = 전체 학생의 수
+
 def solution(n, lost, reserve):
-    answer = 0
-    return answer
+    n = int(input())
+    lost = list(map(int, input()))
+    reserve = list(map(int, input()))
+    result = []
+
+    for i in range(n):
+        if ((lost[i] + 1) in reserve[i]) or ((lost[i] - 1) in reserve[i]):
+            result.append(lost[i])
+        elif reserve:
+            result.append(reserve[i])
+
+        result.sort()
+
+    return set(result)
