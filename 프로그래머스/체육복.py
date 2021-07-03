2393b1ccf99e507dd47b91cdc7b9705f75ef94b0
Elina <elinapark0818@gmail.com>
2021-07-03 20:58:18 +0900

    체육복 맘에 드는 풀이

--- 프로그래머스/체육복.py
+++ 프로그래머스/체육복.py
@@ -1,6 +1,6 @@
 # n = 전체 학생의 수
 
-# abs() 이용
+# abs() 이용 : 그냥..했나보다 굳이 절대값을?
 
 # def solution(n, lost, reserve):
 #     answer = n
@@ -22,21 +22,36 @@
 #     return answer
 
 
-# set 이용
+# # set 이용
+#
+# def solution(n, lost, reserve):
+#     # 여벌의 체육복을 가진 set r를 구한다.
+#     s = set(lost) & set(reserve)
+#     # 체육복 없는 set l을 구한다.
+#     l = set(lost) - s
+#     r = set(reserve) - s
+#
+#     # 앞번호에게 먼저 빌려주기 위해 r을 정렬하여 탐색한다.
+#     for i in sorted(r):
+#         if i - 1 in l:
+#             # 체육복을 받은 번호는 l에서 제거한다.
+#             l.remove(i - 1)
+#         elif i + 1 in l:
+#             l.remove(i + 1)
+#     # 전체 수 n 에서 l의 길이만큼 뺀 값을 반환한다.
+#     return n - len(l)
+
 
 def solution(n, lost, reserve):
-    # 여벌의 체육복을 가진 set r를 구한다.
-    s = set(lost) & set(reserve)
-    # 체육복 없는 set l을 구한다.
-    l = set(lost) - s
-    r = set(reserve) - s
+    s_reserve = set(reserve) - set(lost)
+    s_lost = set(lost) - set(reserve)
 
-    # 앞번호에게 먼저 빌려주기 위해 r을 정렬하여 탐색한다.
-    for i in sorted(r):
-        if i - 1 in l:
-            # 체육복을 받은 번호는 l에서 제거한다.
-            l.remove(i - 1)
-        elif i + 1 in l:
-            l.remove(i + 1)
-    # 전체 수 n 에서 l의 길이만큼 뺀 값을 반환한다.
-    return n - len(l)
+    for i in s_reserve:
+        # 왼쪽부터 있으면 빌려주기
+        if (i - 1) in s_lost:
+            s_lost.remove(i - 1)
+        # 왼쪽이 없으면 오른쪽으로 빌려주게끔
+        elif (i + 1) in s_lost:
+            s_lost.remove(i + 1)
+
+    return n - len(s_lost)
