b28d5fa4651e46c5efc85b442f7270aee32781ca
Elina <elinapark0818@gmail.com>
2021-07-03 21:32:01 +0900

    이게 더 좋다

--- 프로그래머스/소수 찾기.py
+++ 프로그래머스/소수 찾기.py
@@ -1,17 +1,19 @@
-# def solution(n):
-#     answer = set([i for i in range(3, n + 1, 2)])
-#
-#     for i in range(3, n + 1, 2):
-#         if i in answer:
-#             answer -= set([i for i in range(i * 2, n + 1, i)])
-#
-#     return len(answer) + 1
-
-
 def solution(n):
-    num = set(range(2, n + 1))
+    answer = set([i for i in range(3, n + 1, 2)])
 
-    for i in range(2, n + 1):
-        if i in num:
-            num -= set(range(2 * i, n + 1, i))
-    return len(num)
+    for i in range(3, n + 1, 2):
+        if i in answer:
+            answer -= set([i for i in range(i * 2, n + 1, i)])
+
+    return len(answer) + 1
+
+
+# def solution(n):
+#     num = set(range(2, n + 1))
+#
+#     for i in range(2, n + 1):
+#         if i in num:
+#             num -= set(range(2 * i, n + 1, i))
+#     return len(num)
+
+print(solution(1000000))
