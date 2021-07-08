e687d1f980314be469d6988f8f92ba68cba9d265
Elina <elinapark0818@gmail.com>
2021-07-08 23:38:05 +0900

    소수 구하기(1929)

--- 프로그래머스/소수 찾기.py
+++ 프로그래머스/소수 찾기.py
@@ -1,3 +1,6 @@
+import sys
+
+
 def solution(n):
     answer = set([i for i in range(3, n + 1, 2)])
 
@@ -18,6 +21,7 @@ def solution(n):
 
 print(solution(1000000))
 
+
 def solution(n):
     num = set(range(2, n + 1))
 
@@ -25,3 +29,23 @@ def solution(n):
         if i in num:
             num -= set(range(2 * i, n + 1, i))
     return len(num)
+
+
+# ----------------------------------------
+
+M, N = map(int, sys.stdin.readline().split())
+li = []
+for i in range(M, N + 1):
+    if i == 1:
+        pass
+    elif i == 2:
+        li.append(i)
+    else:
+        for j in range(2, i):
+            if i % j == 0:
+                break
+            elif j == i - 1:
+                li.append(i)
+
+for i in li:
+    print(i)
