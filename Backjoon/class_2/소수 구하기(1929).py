b534cfb2430b59a25f69b422f8edd2594b9e5809
Elina <elinapark0818@gmail.com>
2021-07-08 23:41:03 +0900

    소수 구하기(1929)

--- Backjoon/class_2/소수 구하기(1929).py
+++ Backjoon/class_2/소수 구하기(1929).py
@@ -1,14 +1,18 @@
-def isPrime(num):
-    if num==1:
+import sys
+
+
+def solution(num):
+    if num == 1:
         return False
     else:
-        for i in range(2, int(num**0.5)+1):
-            if num%i == 0:
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
                 return False
         return True
 
-M, N = map(int, input().split())
 
-for i in range(M, N+1):
-    if isPrime(i):
+M, N = map(int, sys.stdin.readline().split())
+
+for i in range(M, N + 1):
+    if solution(i):
         print(i)
