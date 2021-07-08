c1bed19d540ff77ec61ee681827e34318a2d98f8
Elina <elinapark0818@gmail.com>
2021-07-08 23:32:10 +0900

    프린터 큐 deque

--- Backjoon/class_2/프린터 큐(1996).py
+++ Backjoon/class_2/프린터 큐(1996).py
@@ -25,6 +25,8 @@
 #             ipt_.append(ipt_[0])
 #             del ipt_[0]
 
+# --------------------------------------
+
 test_cases = int(input())
 
 for _ in range(test_cases):
@@ -50,3 +52,26 @@ for x in imp:
     else:
         imp.append(imp.pop(0))
         idx.append(idx.pop(0))
+
+# -------------------------------------------
+from collections import deque
+from sys import stdin
+
+num = int(stdin.readline())
+for _ in range(num):
+    N, M = map(int, stdin.readline().split())
+    que = deque(map(int, stdin.readline().split()))
+    cnt = 0
+    while que:
+        top = max(que)
+        M -= 1
+        pop = que.popleft()
+        if top != pop:
+            que.append(pop)
+            if M < 0:
+                M = len(que) - 1
+        else:
+            cnt += 1
+            if M == -1:
+                print(cnt)
+                break
