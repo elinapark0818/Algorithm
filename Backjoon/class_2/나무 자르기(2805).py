f7be893d4784b5a8f1da402d609880dabca031d9
Elina <elinapark0818@gmail.com>
2021-07-09 23:20:06 +0900

    막 써보기

--- Backjoon/class_2/나무 자르기(2805).py
+++ Backjoon/class_2/나무 자르기(2805).py
@@ -2,4 +2,15 @@ import sys
 
 N, M = map(int, sys.stdin.readline().split())
 for i in range(N):
-    tree = map(int, sys.stdin.readline().split())
+    tree = list(map(int, sys.stdin.readline().split()))
+    H = []
+    for i in range(len(tree)):
+        if max(tree):
+            tree.pop(i)
+        elif min(tree):
+            tree.pop(i)
+        H.append(tree[i])
+        for i in range(len(H)):
+            H_sum = sum(H)
+            result = H_sum % len(H)
+            print(result)
