42ad5a62418c422bfe40a3a9ac205a069cda6495
Elina <elinapark0818@gmail.com>
2021-07-09 23:55:23 +0900

    길이를 먼저 구하고 H 구하기

--- Backjoon/class_2/나무 자르기(2805).py
+++ Backjoon/class_2/나무 자르기(2805).py
@@ -1,19 +1,46 @@
-import sys
+# import sys
+#
+# N, M = map(int, sys.stdin.readline().split())
+# tree = list(map(int, sys.stdin.readline().split()))
+# start, end = 1, max(tree)
+#
+# while start <= end:
+#     avg = (start + end) // 2
+#
+#     tree_sum = 0
+#     for i in tree:
+#         if i >= avg:
+#             tree_sum += i - avg
+#
+#     if tree_sum >= M:
+#         start = avg + 1
+#     else:
+#         end = avg - 1
+# print(end)
 
-N, M = map(int, sys.stdin.readline().split())
-tree = list(map(int, sys.stdin.readline().split()))
-start, end = 1, max(tree)
 
-while start <= end:
-    avg = (start + end) // 2
+# 통과되는 풀이
+N, M = map(int, input().split())
+trees = list(map(int, input().split()))
 
-    tree_sum = 0
-    for i in tree:
-        if i >= avg:
-            tree_sum += i - avg
+left = 0
+right = max(trees)
+result = []
 
-    if tree_sum >= M:
-        start = avg + 1
+while not right < left:
+    length = 0
+    mid = (left + right) // 2
+
+    length = sum(i - mid if i > mid else 0 for i in trees)
+
+    if length == M:
+        result.append(mid)
+        break
+    elif length > M:
+        result.append(mid)
+        left = mid + 1
     else:
-        end = avg - 1
-print(end)
+        right = mid - 1
+H = max(result)
+
+print(H)
