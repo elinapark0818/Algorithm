4c99cec6732e70ff1b6b66086da68b08ae39bc35
Elina <elinapark0818@gmail.com>
2021-07-09 23:32:00 +0900

    나무가 M보다는 커야한다

--- Backjoon/class_2/나무 자르기(2805).py
+++ Backjoon/class_2/나무 자르기(2805).py
@@ -5,9 +5,9 @@ for i in range(N):
     tree = list(map(int, sys.stdin.readline().split()))
     H = []
     for i in range(len(tree)):
-        tree.remove(max(tree))
-        tree.remove(min(tree))
-
+        if tree[i] > M:
+            H.append(tree[i])
+            print(H)
 
         #     tree.pop(i)
         # elif min(tree):
