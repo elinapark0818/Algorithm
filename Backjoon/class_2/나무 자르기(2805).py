af8a55c44f4de2a767b6292b628ede5a2f5c1b4c
Elina <elinapark0818@gmail.com>
2021-07-09 23:45:28 +0900

    이분탐색!

--- Backjoon/class_2/나무 자르기(2805).py
+++ Backjoon/class_2/나무 자르기(2805).py
@@ -1,19 +1,19 @@
 import sys
 
 N, M = map(int, sys.stdin.readline().split())
-for i in range(N):
-    tree = list(map(int, sys.stdin.readline().split()))
-    H = []
-    for i in range(len(tree)):
-        if tree[i] > M:
-            H.append(tree[i])
-            print(H)
+tree = list(map(int, sys.stdin.readline().split()))
+start, end = 1, max(tree)
 
-        #     tree.pop(i)
-        # elif min(tree):
-        #     tree.pop(i)
-        # H.append(tree)
-        # for i in range(len(H)):
-        #     H_sum = sum(H[i])
-        #     result = H_sum % len(H)
-        #     print(result)
+while start <= end:
+    avg = (start + end) // 2
+
+    tree_sum = 0
+    for i in tree:
+        if i >= avg:
+            tree_sum += i - avg
+
+    if tree_sum >= M:
+        start = avg + 1
+    else:
+        end = avg - 1
+print(end)
