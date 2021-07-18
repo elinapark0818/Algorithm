ccbe624447ad7e9e976c59b7bbb242aaff6b0bf3
Elina <elinapark0818@gmail.com>
2021-07-18 22:04:43 +0900

    .

--- Backjoon/class_2/나무 자르기(2805).py
+++ Backjoon/class_2/나무 자르기(2805).py
@@ -47,29 +47,29 @@ print(H)
 
 # Counter 이용한 풀이
 
-from sys import stdin
-from collections import Counter
-
-
-def BS_tree(data, M):
-    start, end = 0, max(data)
-    result = 0
-    while start <= end:
-        total = 0
-        mid = (start + end) // 2
-        for x, num in data.items():
-            if x > mid:
-                total += (x - mid) * num  # 해당 값이 등장한 개수만큼 곱한다
-
-        if total < M:
-            end = mid - 1
-        else:
-            result = mid
-            start = mid + 1
-
-    return result
-
-
-_, M = map(int, stdin.readline().split())
-data = Counter(map(int, stdin.readline().split()))
-print(BS_tree(data, M))
+# from sys import stdin
+# from collections import Counter
+#
+#
+# def BS_tree(data, M):
+#     start, end = 0, max(data)
+#     result = 0
+#     while start <= end:
+#         total = 0
+#         mid = (start + end) // 2
+#         for x, num in data.items():
+#             if x > mid:
+#                 total += (x - mid) * num  # 해당 값이 등장한 개수만큼 곱한다
+#
+#         if total < M:
+#             end = mid - 1
+#         else:
+#             result = mid
+#             start = mid + 1
+#
+#     return result
+#
+#
+# _, M = map(int, stdin.readline().split())
+# data = Counter(map(int, stdin.readline().split()))
+# print(BS_tree(data, M))
